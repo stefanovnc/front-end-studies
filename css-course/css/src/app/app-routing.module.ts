54c9d4eb14bd8332451d0d3fb302a3b66d2da0a7
stefanovnc <stefano_gajj@outlook.com>
2024-03-22 00:56:16 -0300

    :construction: building: Header - Home Page

--- css-course/css/src/app/app-routing.module.ts
+++ css-course/css/src/app/app-routing.module.ts
@@ -3,14 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
